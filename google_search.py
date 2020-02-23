a1d28333623fc8689f95980f635f754dd95c0f72
MikeyPPPP <50926811+MikeyPPPPPPPP@users.noreply.github.com>
2020-02-23 04:35:54 -0800

    Update google_search.py

--- google_search.py
+++ google_search.py
@@ -1,17 +1,1 @@
-import google_module
 
-
-
-site_counter = []
-sites = []
-
-google_module.get_urls()
-
-
-for x in google_module.get_pages_at_bottom():
-    sites.append(x)
-    
-#print(sites)
-print(google_module.get_page_nums())
-print(google_module.get_page_n_on())
-print(google_module.get_searches())
